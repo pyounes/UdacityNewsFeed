4fb67156bea0329fef26f347343ef812ba1d0d0e
Pierre Younes <pyounes@gmail.com>
2021-05-15 17:54:16 +0300

    Adding Core Data Model

--- NewsFeed/Controllers/StoredNewsVC.swift
+++ NewsFeed/Controllers/StoredNewsVC.swift
@@ -6,13 +6,27 @@
 //
 
 import UIKit
+import CoreData
 
 class StoredNewsVC: UIViewController {
+    
+    // Variables
+    var dataController: DataController!
+    var feeds: [Feed] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Stored News"
         view.backgroundColor = .systemBackground
+    }
+    
+    // MARK: - Customer Methods
+    // fetch Saved Articles from Coredata
+    fileprivate func fetchArticles() {
+        let fetchRequest: NSFetchRequest<Feed> = Feed.fetchRequest()
+        if let result = try? dataController.viewContext.fetch(fetchRequest) {
+            feeds = result
+        }
     }
     
 
